e2a715a83b4c570d65a25427f8e46d2c3998e4b1
X-TECH CORP <zabsonrejacques30@gmail.com>
2025-11-13 13:40:13 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,16 +1,16 @@
 const settings = {
   packname: 'Knight Bot',
-  author: '‎',
+  author: 'DR XENON',
   botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  botOwner: 'XENON', // Your name
+  ownerNumber: '2250501758422', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
   storeWriteInterval: 10000,
   description: "This is a bot for managing group commands and automating tasks.",
   version: "2.2.2",
-  updateZipUrl: "https://github.com/mruniquehacker/Knightbot-MD/archive/refs/heads/main.zip",
+  updateZipUrl: "https://github.com/DrXenon2/Knightbot-MDarchive/refs/heads/main.zip",
 };
 
 module.exports = settings;
